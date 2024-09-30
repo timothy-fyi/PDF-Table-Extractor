e027490a05ad4a85caa7bd03066ed6fe8d0a0772
Tim F <timothy.flanagan@youngwilliams.com>
2024-09-30 16:31:36 -0400

    Added additional error handling and Stdout control

--- pdf_extract_utils.py
+++ pdf_extract_utils.py
@@ -26,7 +26,7 @@ def extract_and_export_tables(file, config, pages):
     date_extract = file[-8:-4]
 
     try:
-        tables = camelot.read_pdf(file, flavor='stream', pages=pages)
+        tables = camelot.read_pdf(file, flavor='stream', pages=pages, suppress_stdout=True)
     except IndexError:
         print('Pages not found. Did you input the correct page numbers?')
         exit()
@@ -36,7 +36,17 @@ def extract_and_export_tables(file, config, pages):
 
     for i, folder in enumerate(folders):
         file_path = os.path.join(base_export_path, folder, f'{folder.lower().replace(" ", "_")}{date_extract}.csv')
-        tables[i].to_csv(file_path)
+
+        try:
+            tables[i].to_csv(file_path)
+        except IndexError:
+            print(
+                f'ERROR: EXTRACT NOT SUCCESSFUL!\n'
+                f'REASON: Pages in {file_name} seem to be image based.\n' 
+                f'SOLUTION: Please check {file_name} and any other PDFs in the "Unprocessed" folder '
+                f'and ensure that they are in the correct format, i.e. text based and NOT screenshot/image based.'
+            )
+            exit()
 
     print(f'Moving {file_name} to processed folder...')
     shutil.move(os.path.join(config['unprocessed_folder'], file_name), os.path.join(config['processed_folder'], file_name))
